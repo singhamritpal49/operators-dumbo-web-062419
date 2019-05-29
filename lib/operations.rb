4fddd745e24271ede3e2e12e6332b2a9b0675f3d
Amritpal Singh <singhamritpal49@gmail.com>
2019-05-29 15:15:05 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 def unsafe?(speed)
-
+if speed > 60 || speed < 40
+	return true
+else speed >= 40 && speed <= 60
+	return false
 end
-
-
-
+end
 def not_safe?(speed)
-	
+#conditional ? action_if_true : action_if_false
+speed > 60 || speed < 40 ? true : false
+
 end
-	
-
-
