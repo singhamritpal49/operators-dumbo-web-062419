a326e92e59f0a53ed34fb8e7bfc26eb69f120665
Amritpal Singh <singhamritpal49@gmail.com>
2019-05-29 14:58:46 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,5 @@
-def unsafe?(speed)
-if speed > 60 || speed < 40
-	return true
-else speed > 40 && speed < 60
-	return false
-end
+def not_safe?(speed)
+  speed < 40 || speed > 60 ? true : false
 end
 
 def not_safe?(speed)
