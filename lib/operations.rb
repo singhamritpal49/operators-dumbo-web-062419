6eb383223270104495adb2d5b86ac22f64360dd9
Amritpal Singh <singhamritpal49@gmail.com>
2019-05-29 15:05:49 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,14 @@
 def unsafe?(speed)
-	def unsafe?(speed)
-
-	   if speed < 40 || speed > 60
-	    return true
-	  else
-	    return false
-	  end
-	end	end
+if speed > 60 || speed < 40
+	return true
+else speed >+ 40 && speed <= 60
+	return false
+end
+end
 
 def not_safe?(speed)
+	age = =
+
+	age < 2 ? "baby" : "not a baby"
 
 end
